dbc08593eecd3d616df14abfc803a45c99b63b74
samacluka <samacl@mcmaster.ca>
2017-10-14 18:00:31 -0700

    Intial User Login GUI
    A GUI for logging in as a given user.

---
+++ UserLogin/UserLogin/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Uname = New System.Windows.Forms.TextBox()
+        Me.Pword = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Username:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 72)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Password"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(300, 108)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Continue"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Uname
+        '
+        Me.Uname.Location = New System.Drawing.Point(78, 31)
+        Me.Uname.Name = "Uname"
+        Me.Uname.Size = New System.Drawing.Size(229, 20)
+        Me.Uname.TabIndex = 3
+        '
+        'Pword
+        '
+        Me.Pword.Location = New System.Drawing.Point(78, 69)
+        Me.Pword.Name = "Pword"
+        Me.Pword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.Pword.Size = New System.Drawing.Size(229, 20)
+        Me.Pword.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(387, 143)
+        Me.Controls.Add(Me.Pword)
+        Me.Controls.Add(Me.Uname)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Uname As TextBox
+    Friend WithEvents Pword As TextBox
+End Class
